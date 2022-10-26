94c9b8b4dd750efd337ca0fc875d231fd7d37524
Abdul-Rauf Adebayo <61793402+Scofield1@users.noreply.github.com>
2022-10-26 17:19:35 +0100

    Update settings.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -119,7 +119,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+CSRF_TRUSTED_ORIGINS = ['https://web-production-8168.up.railway.app', 'https://*.127.0.0.1']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
